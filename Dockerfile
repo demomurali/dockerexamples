bd5cce12736b2bfc6f69c556e08d16e795711b88
Murali <murali@Muralis-MacBook-Air.local>
2018-09-21 21:34:15 +0530

    .gitignore added

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM alpine
+#RUN echo helloworld
+CMD echo $PATH
---
+++ mysqlProj/dump.sql
@@ -0,0 +1,6 @@
+    create table employeecomplete(id int auto_increment primary key, 
+                                name varchar(30),
+                                salary decimal(10,2),
+                                age int);
+
+    insert into employeecomplete values(1001,'ramesh',3434.34,45);
